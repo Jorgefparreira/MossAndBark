374352c24d7ce975b4edc259ccececd3d9b89914
Jorge <jorgefparreira@gmail.com>
2024-03-22 15:53:27 +0000

    new colour scheme

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,19 @@
-const admin = require('firebase-admin');
-const functions = require('firebase-functions');
-const contactMailer = require('./contactMailer.js');
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
-admin.initializeApp();
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
 
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
 
-exports.contactMailer = functions.https.onRequest((req, res) => {
-  contactMailer.handler(req, res);
-});
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
